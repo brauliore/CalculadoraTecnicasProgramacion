ad75b6021011887f20860e2383210e77e31f2879
brauliore <brauliore@gmail.com>
2022-09-20 19:33:24 -0600

    Calculadora
    Este es el commit inicial de la calculadora

---
+++ Carro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Carro
+    {
+        public Engine motor { get; set; }
+        public int cantidadDeRuedas { get; set; }
+
+    }
+
+    internal class Engine
+    {
+        public int vujias { get; set; }
+    }
+}
